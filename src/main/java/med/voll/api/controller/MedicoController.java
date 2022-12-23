d121d4640b23e71fbb2040058b693c0fcbab69b2
Gui <guilherme.g.braz@outlook.com>
2022-12-23 11:50:04 -0300

    Refactor Medico Controller, JpaMedicoRepository

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,9 +1,14 @@
 package med.voll.api.controller;
 
 import med.voll.api.controller.model.dto.MedicoDTO;
+import med.voll.api.core.entities.BusinessException;
 import med.voll.api.dataprovider.entities.MedicoEntity;
-import med.voll.api.dataprovider.medico.JpaMedicoDAO;
+import med.voll.api.dataprovider.medico.JpaMedicoRepository;
+import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @Autowired
-    JpaMedicoDAO jpaMedicoRepository;
+    JpaMedicoRepository jpaMedicoRepository;
 
     @PostMapping
-    public void Cadastrar(@RequestBody MedicoDTO jsonMedico) {
+    public ResponseEntity<String> Cadastrar(@RequestBody MedicoDTO jsonMedico, Request req) {
         MedicoEntity md = new MedicoEntity(jsonMedico.toMedico());
-        jpaMedicoRepository.save(md);
-        System.out.println(jsonMedico);
+        try{
+            jpaMedicoRepository.save(md);
+        }catch (BusinessException exception){
+            return new ResponseEntity<>(exception.getCause() + exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+
+        return new ResponseEntity("Medico Criado com sucesso", HttpStatus.CREATED);
     }
 }
