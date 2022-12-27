258661c5befe8f707f21063a50488b596d2a9054
Gui <guilherme.g.braz@outlook.com>
2022-12-27 14:24:41 -0300

    Listagem dados medico

--- src/main/java/med/voll/api/controller/model/viewModel/DadosListagemMedico.java
+++ src/main/java/med/voll/api/controller/model/viewModel/DadosListagemMedico.java
@@ -4,13 +4,12 @@ package med.voll.api.controller.model.viewModel;
 import med.voll.api.core.entities.medico.Especialidade;
 import med.voll.api.dataprovider.entities.MedicoEntity;
 
-import java.util.Locale;
 
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id,String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosListagemMedico(MedicoEntity medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCRM(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCRM(), medico.getEspecialidade());
     }
 
 }
