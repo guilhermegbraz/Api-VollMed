3edd2a4c1300bb591fd57e8a2367c5bb8a229cb2
Gui <guilherme.g.braz@outlook.com>
2022-12-28 15:38:23 -0300

    Jpa Repository update medico

--- src/main/java/med/voll/api/dataprovider/medico/JpaMedicoDAO.java
+++ src/main/java/med/voll/api/dataprovider/medico/JpaMedicoDAO.java
@@ -1,11 +1,14 @@
 package med.voll.api.dataprovider.medico;
 
 
+import lombok.NonNull;
 import med.voll.api.core.entities.BusinessException;
 import med.voll.api.core.entities.medico.Medico;
 import med.voll.api.core.entities.medico.MedicoRepository;
+import med.voll.api.dataprovider.converters.MedicoEntityToMedicoConversor;
+import med.voll.api.dataprovider.converters.MedicoToMedicoEntityConversor;
 import med.voll.api.dataprovider.entities.MedicoEntity;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -15,24 +18,40 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class JpaMedicoDAO implements MedicoRepository {
 
-    @Autowired
     private final JpaMedicoRepository repository;
 
-    public JpaMedicoDAO(JpaMedicoRepository repository) {
+    private final MedicoToMedicoEntityConversor medicoToMedicoEntityConverter;
+    private final MedicoEntityToMedicoConversor medicoEntityToMedicoConversor;
+
+    public JpaMedicoDAO(JpaMedicoRepository repository, MedicoToMedicoEntityConversor conversor, MedicoEntityToMedicoConversor medicoEntityToMedicoConversor) {
         this.repository = repository;
+        this.medicoToMedicoEntityConverter = conversor;
+        this.medicoEntityToMedicoConversor = medicoEntityToMedicoConversor;
     }
 
    @Transactional
     @Override
     public void cadastrar(Medico novoMedico) {
-       MedicoEntity medico = new MedicoEntity(novoMedico);
+       MedicoEntity medico = this.medicoToMedicoEntityConverter.executar(novoMedico);
        try{repository.save(medico);
        } catch (Exception e) {
             throw new BusinessException(BusinessException.getFullMessage(e));
        }
    }
 
+    @Override
+    public Medico recuperarUmMedico(Long id) {
+        MedicoEntity medico = this.repository.findById(id).get();
+        return this.medicoEntityToMedicoConversor.executar(medico);
+    }
+
     public Page<MedicoEntity> listarTodosMedicos(Pageable pageable) {
         return this.repository.findAll(pageable);
+    }
+
+
+    @Transactional
+    public void atualizar(@NonNull Medico medico){
+        this.repository.update(medico.getNome(), medico.getTelefone(), medico.getEndereco(), medico.getId());
     }
 }
--- src/main/java/med/voll/api/dataprovider/medico/JpaMedicoRepository.java
+++ src/main/java/med/voll/api/dataprovider/medico/JpaMedicoRepository.java
@@ -1,11 +1,17 @@
 package med.voll.api.dataprovider.medico;
 
 
+
+import med.voll.api.core.entities.Endereco;
 import med.voll.api.dataprovider.entities.MedicoEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
@@ -13,4 +19,9 @@ public interface JpaMedicoRepository extends JpaRepository<MedicoEntity, Long> {
 
 
     Page<MedicoEntity> findAll(Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Medico m SET m.nome = :nomeNovo, m.telefone = :telefoneNovo, m.endereco = :enderecoNovo WHERE m.id = :id")
+    void update(@Param("nomeNovo") String nome, @Param("telefoneNovo") String telefone, @Param("enderecoNovo") Endereco endereco, @Param("id") Long id);
 }
