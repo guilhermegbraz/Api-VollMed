97d94f78090c2ef2342da1186a1213e7338f4ccb
Gui <guilherme.g.braz@outlook.com>
2023-02-15 17:27:09 -0300

    Implementação do Authentication Service

---
+++ src/main/java/med/voll/api/entryponit/api_rest/security/autentication/AutenticationService.java
@@ -0,0 +1,22 @@
+package med.voll.api.entryponit.api_rest.security.autentication;
+
+import med.voll.api.dataprovider.autentication.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticationService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AutenticationService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+}
