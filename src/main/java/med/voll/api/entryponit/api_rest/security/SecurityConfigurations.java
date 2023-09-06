ce658cd4e285d4564a42e94966241c8e0e89a787
Gui <guilherme.g.braz@outlook.com>
2023-09-05 22:16:06 -0300

    documentação API

--- src/main/java/med/voll/api/entryponit/api_rest/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/entryponit/api_rest/security/SecurityConfigurations.java
@@ -21,14 +21,14 @@ public class SecurityConfigurations {
     @Autowired
     private SecurityFilter securityFilter;
 
-
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "//swagger-ui/**").permitAll();
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
